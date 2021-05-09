3d5ca4c89b362db5191afd6b08705f94ccffb907
CyprienGille <83751996+CyprienGille@users.noreply.github.com>
2021-05-10 00:43:22 +0200

    Uploading matlab scripts

---
+++ run.m
@@ -0,0 +1,26 @@
+%%%%%%%%%%%%%%%%%%%%%%%% Explicite
+%% Cas 1, chambre 1, stat
+% poisson_stat1(0, 20, 20, 40);
+%% Cas 1, chambre 2, stat
+% poisson_stat2(0, 20, 20, 40);
+
+%% Cas 2, chambre 1, stat
+% poisson_stat1(0, 15, -10, 40);
+%% Cas 2, chambre 2, stat
+% poisson_stat2(0, 15, -10, 40);
+
+%% Cas 3, chambre 1, stat
+% poisson_stat1(500, 15, -10, 40);
+%% Cas 3, chambre 2, stat
+% poisson_stat2(500, 15, -10, 40);
+
+
+%%%%%%%%%%%%%%%%% Implicite 
+%% Cas 3, chambre 2, implicite
+%poisson_instat2_imp(500, 15, -10, 40);
+
+%% Cas 4, chambre 1, implicite
+poisson_instat1_imp(-195, 20, 35, 30);
+
+
+
